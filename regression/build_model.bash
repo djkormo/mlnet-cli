29edb83bbe573f0616896fe1002f0ea381e38b34
djkormo <kpudlowski@plus.pl>
2019-05-24 11:52:41 +0200

    adding USA_Housing.csv dataset for regression

--- regression/build_model.bash
+++ regression/build_model.bash
@@ -1,4 +1,4 @@
 # root directory of mlnet tool 
 DIR=$HOME/.dotnet/tools
 # go 
-$DIR/mlnet auto-train --task regression --dataset "AgeRangeData03_AgeGenderLabelEncodedMoreData.csv" --label-column-index 2 --has-header true --max-exploration-time 60
+$DIR/mlnet auto-train --task regression --dataset "USA_Housing 	.csv" --label-column-index 5 --has-header true --max-exploration-time 60
