42662176dc29ca5498655c8f525af9f3bd25b33e
djkormo <kpudlowski@plus.pl>
2019-05-24 15:09:55 +0200

    adding deploy_model_to_storage.bash

--- deploy_model_to_storage.bash
+++ deploy_model_to_storage.bash
@@ -40,43 +40,60 @@ git pull
 # weryfikujemy niespójności zawartości lokalnego i zdalnego repozytorium 
 git status
 
-
-
-az group create --name $ML_GROUP --location $ML_LOCATION
+#if [[ az group exists -n $ML_GROUP ]]; 
+if [ $(az group exists -n $ML_GROUP) == 'true' ];
+then
+  echo "$ML_GROUP grupa zasobow istnieje"
+  
+else
+   az group create --name $ML_GROUP --location $ML_LOCATION
+fi 
 
 # tu sprawdzenie czy musimy tworzyć konto w ramach grupy $ML_GROUP
 
 
-ML_STACCOUNT_CURR=(az storage account list -g $ML_GROUP --query "[0].name" |grep "staccmlnet")
-if [ -z "$var" ]
+ML_STACCOUNT_CURR=$(az storage account list -g $ML_GROUP --query "[0].name" | tr -d '"'|grep "staccmlnet")
+# sprawdzamy czy konto nie zalostalo zalozone wczesniej ]
+if [ -z "$ML_STACCOUNT_CURR" ]
 
 then
 
-  ML_STACCOUNT=$ML_STACCOUNT_CURR
+  echo "$ML_STACCOUNT_CURR konto nie istnieje"
+  
   az storage account create --name $ML_STACCOUNT --location $ML_LOCATION --resource-group $ML_GROUP --sku Standard_LRS
 
-   az functionapp create --name $ML_FUNCAPP --storage-account $ML_STACCOUNT --consumption-plan-location $ML_LOCATION --resource-group $ML_GROUP
+  az functionapp create --name $ML_FUNCAPP --storage-account $ML_STACCOUNT --consumption-plan-location $ML_LOCATION --resource-group $ML_GROUP
 
-   az functionapp config appsettings set --name $ML_FUNCAPP --resource-group $ML_GROUP --settings FUNCTIONS_EXTENSION_VERSION=beta
+  az functionapp config appsettings set --name $ML_FUNCAPP --resource-group $ML_GROUP --settings FUNCTIONS_EXTENSION_VERSION=beta
+
+  az storage account create --name $ML_STACCOUNT --location $ML_LOCATION --resource-group $ML_GROUP --sku Standard_LRS
+
+  az functionapp create --name $ML_FUNCAPP --storage-account $ML_STACCOUNT --consumption-plan-location $ML_LOCATION --r#esource-group $ML_GROUP
+
+  az functionapp config appsettings set --name $ML_FUNCAPP --resource-group $ML_GROUP --settings FUNCTIONS_EXTENSION_VERSION=beta
+
+  az storage account keys list --account-name $ML_STACCOUNT --resource-group $ML_GROUP
+
+else
+  
+   echo "$ML_STACCOUNT_CURR konto istnieje"
+   
+   
 fi
 
+# list w naszej grupie zasobow 
 az storage account list -g $ML_GROUP -o table 
 
-#az storage account create --name $ML_STACCOUNT --location $ML_LOCATION --resource-group $ML_GROUP --sku Standard_LRS
+# pobieramy glowny klucz 
+ACC_KEY=$(az storage account keys list --resource-group $ML_GROUP --account-name $ML_STACCOUNT_CURR --query "[0].value" | tr -d '"')
 
-#az functionapp create --name $ML_FUNCAPP --storage-account $ML_STACCOUNT --consumption-plan-location $ML_LOCATION --r#esource-group $ML_GROUP
+# tworzymy kontener  
+az storage container create --name models --account-key $ACC_KEY --account-name $ML_STACCOUNT_CURR
 
-#az functionapp config appsettings set --name $ML_FUNCAPP --resource-group $ML_GROUP --settings FUNCTIONS_EXTENSION_VERSION=beta
+az storage blob upload --account-name $ML_STACCOUNT_CURR  --account-key $ACC_KEY -c "models" -f $ML_MODEL_FILE -n $ML_MODEL_BLOB
 
-#az storage account keys list --account-name $ML_STACCOUNT --resource-group $ML_GROUP
+# lista kontenerow 
+az storage container list --account-name $ML_STACCOUNT_CURR --output table
 
-
-#ACC_KEY=$(az storage account keys list --resource-group $ML_GROUP --account-name $ML_STACCOUNT --query "[0].value" | tr -d '"')
-
-#az storage container create --name models --account-key $ACC_KEY --account-name $ML_STACCOUNT
-
-#az storage blob upload --account-name $ML_STACCOUNT  --account-key $ACC_KEY -c "models" -f $ML_MODEL_FILE -n $ML_MODEL_BLOB
-
-#az storage blob list --container-name models --account-name $ML_STACCOUNT --output table
-
-
+# lista blobow w kontenerze
+az storage blob list --container-name models --account-name $ML_STACCOUNT_CURR --output table
