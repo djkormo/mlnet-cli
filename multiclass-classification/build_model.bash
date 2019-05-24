c40c0eca23f90262ff601613784f5869ac98d066
djkormo <kpudlowski@plus.pl>
2019-05-24 12:21:19 +0200

    correction of multiclass-classification/build_model.bash

--- multiclass-classification/build_model.bash
+++ multiclass-classification/build_model.bash
@@ -1,4 +1,5 @@
-# root directory of mlnet tool 
+#root directory of mlnet tool 
 DIR=$HOME/.dotnet/tools
-go 
+
+#go 
 $DIR/mlnet auto-train --task multiclass-classification --dataset "iris.csv" --label-column-index 4 --has-header true --max-exploration-time 60
